74d846d9f8d1c6828a21328f9f4e5a690979e4f2
장상민 <jangsangmin0401@gmail.com>
2026-05-08 18:10:53 +0900

    feat(be):# 교차로 노드 레이어 지도 표시

--- backend/src/main/java/com/smartcane/backend/domain/hazard/entity/HazardType.java
+++ backend/src/main/java/com/smartcane/backend/domain/hazard/entity/HazardType.java
@@ -12,4 +12,17 @@ public enum HazardType {
     CAUTION_ZONE_MANHOLE,
     CAUTION_ZONE_GRATING,
     CAUTION_ZONE_REPAIR_ZONE
+    
+    // 안전신고 클래스들
+//    pole : 기둥
+//    bollard : 볼라드
+//    barricade : 바리케이드
+//    movable_signage : 이동식 표지판
+//    sidewalk:damaged : 손상된 보도
+//    braille_guide_blocks:damaged : 손상된 점자블록
+//    alley:damaged : 손상된 골목길
+//    caution_zone:stairs : 계단 주의구역
+//    caution_zone:manhole : 맨홀 주의구역
+//    caution_zone:grating : 배수구 주의구역
+//    caution_zone:repair_zone : 공사구역 주의구역
 }
